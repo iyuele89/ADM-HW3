b1547dad1f09f0c185b1c59fa41437665c271649
stdrr <development.stdrr@gmail.com>
2020-11-22 17:45:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
 from scripts.data_collection import DataCollector
 
 
-try:
-    block = int(input('Provide a block of pages number to start from (counting from 0 to 2). Write -1 to collect all the pages:\t'))
-except:
-    print('Input not valid: not an integer!')
-    exit(-1)
+all_pages = input('Collect all the pages? (Write n to select a specific range) [y/n]:\t')
 
-try:
-    page = int(input('Provide a page number to start from (counting from 1 to 100):\t'))
-except:
-    print('Input not valid: not an integer!')
-    exit(-1)
 
-try:
-    line = int(input('Provide a line number to start from (counting from 1 to 100).\
-                    \nMind that this is an offset\
-                    \n\nE.g.: if page number=100 and line number = 34, then start from 134th item:\t'))
-except:
-    print('Input not valid: not an integer!')
-    exit(-1)
+if all_pages == 'y':
+    print('\n'*3)
+    dc = DataCollector(all_pages=True)
+    dc.get_html_pages()
+    
+else:
+    try:
+        block = int(input('\nProvide a block of pages number to start from (counting from 0 to 2):\t'))
+    except:
+        print('Input not valid: not an integer!')
+        exit(-1)
 
-print('\n'*3)
+    try:
+        page = int(input('\nProvide a page number to start from (counting from 0 to 99):\t'))
+    except:
+        print('Input not valid: not an integer!')
+        exit(-1)
 
-dc = DataCollector(block_number=block, page_number=page, line_number=line)
-dc.save_html_pages()
+    try:
+        line = int(input('\nProvide a line number to start from (counting from 0 to 99):\t'))
+    except:
+        print('Input not valid: not an integer!')
+        exit(-1)
+    print('\n'*3)
+    dc = DataCollector(block_number=block, page_number=page, line_number=line)
+    dc.get_html_pages()
