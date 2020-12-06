701008087c6f5303a47473f5eb29e8c288e858c5
stdrr <development.stdrr@gmail.com>
2020-12-06 21:34:21 +0100

    Minor changes

--- scripts/build_tsv.py
+++ scripts/build_tsv.py
@@ -3,100 +3,10 @@ from bs4 import BeautifulSoup
 import re
 from utilities import FileContentGetter
 import pandas as pd
-from langdetect import detect
 import spacy
 from spacy_fastlang import LanguageDetector
 from tqdm import tqdm
-import warnings
 
-warnings.filterwarnings("ignore")
-# def book_scraping(html_source): # this takes the html content and returns a list with the useful info
-
-#     soup = BeautifulSoup(html_source, features='lxml') # instantiate a BeautifulSoup object for HTML parsing
-
-#     bookTitle = soup.find_all('h1', id='bookTitle')[0].contents[0].strip() # get the book title
-
-#     # if bookSeries is not present, then set it to the empty string
-#     try:
-#         bookSeries = soup.find_all('h2', id='bookSeries')[0].contents[1].contents[0].strip()[1:-1]
-#     except:
-#         bookSeries = ''
-
-#     # if bookAuthors is not present, then set it to the empty string
-#     try:
-#         bookAuthors = soup.find_all('span', itemprop='name')[0].contents[0].strip()
-#     except:
-#         bookAuthors = ''
-    
-#     # the plot of the book is essential; if something goes wrong with the plot, raise an error
-#     try:
-#         descr = soup.find_all('div', id='description')[0].contents # get the plot
-#         descr_fil= list(filter(lambda s: s!='\n', descr)) # clean it from newline chars
-#         if len(descr_fil) == 1:
-#             Plot = ''.join(descr_fil[0].contents[0]) # join the filtered plot into a string
-#         else:
-#             descr_fil = descr_fil[1:-1]
-#             x = [j for i in descr_fil for j in i.contents if (isinstance(j, str)==True)]
-#             Plot = ''.join(x) # join the filtered plot into a string
-#         if detect(Plot) != 'en':
-#             raise Exception # if the plot is not in english, raise an error
-#     except:
-#         raise # pass the error to the caller function
-
-#     # if NumberofPages is not present, then set it to the empty string
-#     try:
-#         NumberofPages = soup.find_all('span', itemprop='numberOfPages')[0].contents[0].split()[0]
-#     except:
-#         NumberofPages = ''
-    
-#     # if ratingValue is not present, then set it to the empty string
-#     try:
-#         ratingValue = soup.find_all('span', itemprop='ratingValue')[0].contents[0].strip()
-#     except:
-#         ratingValue = ''
-    
-#     # if rating_reviews is not present, then set it to the empty string
-#     try:
-#         ratings_reviews = soup.find_all('a', href='#other_reviews')
-#         for i in ratings_reviews:
-#             if i.find_all('meta',itemprop='ratingCount'):
-#                 ratingCount = i.contents[2].split()[0]
-#             if i.find_all('meta',itemprop='reviewCount'):
-#                 reviewCount = i.contents[2].split()[0]
-#     except:
-#         ratings_reviews = ''
-
-#     # if Published is not present, then set it to the empty string
-#     try:        
-#         pub = soup.find_all('div', class_='row')[1].contents[0].split()[1:4]
-#         Published = ' '.join(pub) # join the list of publishers
-#     except:
-#         Published = ''
-    
-#     # if Character is not present, then set it to the empty string
-#     try:
-#         char = soup.find_all('a', href=re.compile('characters')) # find the regular expression(re) 'characters' within the attribute href 
-#         if len(char) == 0:
-#             Characters = '' # no characters in char
-#         else:
-#             Characters = ', '.join([i.contents[0] for i in char])
-#     except:
-#         Characters = '' # something went wrong with char
-    
-#     # if Setting is not present, then set it to the empty string
-#     try:
-#         sett = soup.find_all('a', href=re.compile('places')) # find the regular expression(re) 'places' within the attribute href 
-#         if len(sett) == 0:
-#             Setting = ''
-#         else:
-#             Setting = ', '.join([i.contents[0] for i in sett])
-#     except:
-#         Setting = '' # something went wrong with Setting
-    
-#     # get the URL to the page
-#     Url = soup.find_all('link', rel='canonical')[0].get('href')
-
-#     return [bookTitle, bookSeries, bookAuthors, ratingValue, ratingCount, reviewCount, Plot, NumberofPages, Published, Characters, Setting, Url]
 
 
 def book_scraping(html_source): # this takes the html content and returns a list with the useful info
--- scripts/search_engine.py
+++ scripts/search_engine.py
@@ -1,7 +1,10 @@
 import json
-from utilities import TextTools
+from scripts.utilities import TextTools
 import heapq
 import math
+import pandas as pd
+
+
 
 class DocScore:
     def __init__(self, doc_score:list):
@@ -23,58 +26,21 @@ class DocScore:
         return hash(self.doc_score[0])
 
 
-def binary_search(arr, x): 
-    low = 0
-    high = len(arr) - 1
-    mid = 0
-  
-    while low <= high: 
-  
-        mid = (high + low) // 2
-  
-        # Check if x is present at mid 
-        if arr[mid] < x: 
-            low = mid + 1
-  
-        # If x is greater, ignore left half 
-        elif arr[mid] > x: 
-            high = mid - 1
-  
-        # If x is smaller, ignore right half 
-        else: 
-            return mid
-  
-    # If we reach here, then the element was not present 
-    return -1
-
-
-def common_el(l1, l2):
-    result = ([], [])
-    l = [l1, l2]
-    idx_min = 0 if len(l1) < len(l2) else 1
-    for i in range(len(l[idx_min])):
-        j = binary_search(l[1 - idx_min], l[idx_min][i])
-        if j != -1:
-            result[idx_min].append(i)
-            result[1 - idx_min].append(j)
-    return result
-
 
 class SearchEngine:
 
     def __init__(self, vocabulary_path='./data/vocabulary.json',\
                 i_index_path='./data/inverted_index_2_1_1.json',\
                 i_index_tfidf_path='./data/inverted_index_2_2_1.json',\
-                doc_magnitude_path='./data/precomputed/doc_magnitude.json'
-                ):
+                doc_magnitude_path='./data/precomputed/doc_magnitude.json'):
         with open(vocabulary_path, 'r') as vocabulary_file:
             self.vocabulary = json.load(vocabulary_file)
         self.i_index_path = i_index_path
         self.i_index_tfidf_path = i_index_tfidf_path
         self.doc_magnitude_path = doc_magnitude_path
 
-
     def select_mode(self, use_tfidf=False):
+        self.use_tfidf = use_tfidf
         if use_tfidf:
             with open(self.i_index_tfidf_path, 'r') as i_index_tfidf_file:
                 self.i_index = json.load(i_index_tfidf_file)
@@ -127,30 +93,134 @@ class SearchEngine:
         # # print(doc_scores0)
         # heapq._heapify_max(doc_scores)
         # print(heapq._heappop_max(doc_scores))
+
+
+    def _search(self, query):
+        query_words = list(set(TextTools.pre_process(query).split(' ')))
+        query_len = len(query_words)
+        try:
+            posting_lists = [self.i_index[str(self.vocabulary[word])] for word in query_words]
+        except:
+            print('No results :(')
+            return []
+        documents = {k:1 for k in posting_lists[0]}
+        for l in posting_lists[1:]:
+            for doc in l:
+                if doc in documents:
+                    documents[doc] += 1
+        return [k for k in documents if documents[k] == query_len]
         
 
-    def search(self, query):
-        query_words = TextTools.pre_process(query).split(' ')
+    def _search_tfidf(self, query):
+        query_words = list(set(TextTools.pre_process(query).split(' ')))
         query_len = len(query_words)
-        posting_lists = [self.i_index[str(self.vocabulary[word])] for word in query_words]
+        try:
+            posting_lists = [self.i_index[str(self.vocabulary[word])] for word in query_words]
+        except:
+            print('No results :(')
+            return []
         documents = {k[0]:[k[1]] for k in posting_lists[0]}
         for l in posting_lists[1:]:
             for doc in l:
                 if doc[0] in documents:
                     documents[doc[0]].append(doc[1])
-        documents = {k:documents[k] for k in documents if len(documents[k]) == query_len}
-        cosine_sim = lambda x: sum(x) / (sum([c**2 for c in x])**0.5 * query_len**0.5)
-        doc_scores = [DocScore([int(k),-cosine_sim(documents[k])]) for k in documents]
+        documents = {k:documents[k] for k in documents.keys() if len(documents[k]) == query_len}
+        with open(self.doc_magnitude_path, 'r') as doc_magnitude_file:
+            doc_magnitude = json.load(doc_magnitude_file)
+        cosine_sim = lambda d_vec, d_mag: round(sum(d_vec) / d_mag, 3)
+        return [DocScore([int(k), -cosine_sim(documents[k], doc_magnitude[str(k)])]) for k in documents]
+
+
+    def _top_k_docs(self, doc_scores, k):
         heapq.heapify(doc_scores)
-        for k in range(5):
-            print(heapq.heappop(doc_scores))
+        len_scores = len(doc_scores)
+        i = 0
+        top_k_docs = []
+        while i < k and i < len_scores:
+            top_k_docs.append(heapq.heappop(doc_scores))
+            i += 1
+        return top_k_docs
+
+
+    def search(self, query, dataset, k=10, new_score=False, additional_query=None):
+        if self.use_tfidf:
+            doc_scores = self._search_tfidf(query)
+            top_k_docs = self._top_k_docs(doc_scores, k)
+            if new_score:
+                result = pd.DataFrame()
+                for d in top_k_docs:
+                    result = pd.concat([result, dataset[dataset['file_num']==d.doc_score[0]][['bookTitle', 'Plot', 'Url']]])
+                top_k_docs = self._new_scoring_fun(result, top_k_docs, additional_query, k)
+            result = pd.DataFrame()
+            for d in top_k_docs:
+                result = pd.concat([result, dataset[dataset['file_num']==d.doc_score[0]][['bookTitle', 'Plot', 'Url']]])
+            result.loc[:, 'Similarity'] = [-d.doc_score[1] for d in top_k_docs]
+            return result
+        else:
+            result = self._search(query)
+            return dataset[dataset['file_num'].isin(result)][['bookTitle', 'Plot', 'Url']].head(10)
+
+    def _word_present(self, words, query):
+        for word in words:
+            if word in query:
+                return 1
+        return 0
+
+    def _score_title(self, title, query):
+        title_words = TextTools.pre_process(title).split(' ')
+        return self._word_present(title_words, query)
 
     
+    def _score_author(self, author, query):
+        try:
+            if author == None or author == '':
+                return 0
+            author_words = TextTools.pre_process(author).split(' ')
+            return self._word_present(author_words, query)
+        except:
+            print('Error author')
+            return 0
+    
+
+    def _score_characters(self, characters, query):
+        try:
+            if characters == None or characters == '':
+                return 0
+            characters_words = TextTools.pre_process(characters).split(' ')
+            return self._word_present(characters_words, query)
+        except:
+            return 0
 
 
-engine = SearchEngine()
-engine.select_mode(use_tfidf=True)
-engine.search('hunger games')
+    def _score_series(self, series, query):
+        try:
+            if series == None or series == '':
+                return 0
+            series_words = TextTools.pre_process(series).split(' ')
+            return self._word_present(series_words, query)
+        except:
+            return 0
+    
+
+    def _new_scoring_fun(self, dataset, top_k_docs, query, k):
+        query_words = set(TextTools.pre_process(query).split(' '))
+        for i in range(k):
+            title_score = self._score_title(dataset.iloc[i]['bookTitle'], query_words)
+            author_score = self._score_author(dataset.iloc[i]['bookAuthors'], query_words)
+            char_score = self._score_characters(dataset.iloc[i]['Characters'], query_words)
+            series_score = self._score_series(dataset.iloc[i]['bookSeries'], query_words)
+            new_score = round(-top_k_docs[i].doc_score[1] * 0.4 + title_score * 0.3 + \
+                        author_score * 0.07 + char_score * 0.05 + series_score * 0.03 + \
+                        (dataset.iloc[i]['ratingValue'] / 5 * (30001 - dataset.iloc[i]['file_num']) / 30000) * 0.15, 3) 
+            top_k_docs[i].doc_score[1] = -new_score
+        return self._top_k_docs(top_k_docs, k)
+        
+
+
+
+# engine = SearchEngine()
+# engine.select_mode(use_tfidf=True)
+# print(sorted(engine.search('survival games')))
 # engine.test()
 
 
@@ -159,3 +229,15 @@ engine.search('hunger games')
 
 # # res = search(l1, l2)
 # print([l2[i] for i in res[1]])
+
+
+# d1 = DocScore([1, 0.4])
+# d2 = DocScore([2, 0.8])
+# d3 = DocScore([3, 0.3])
+# print(sorted([d1,d2,d3]))
+# arr = [d1,d2,d3]
+# print(arr)
+# heapq.heapify(arr)
+# print(heapq.heappop(arr))
+# print(heapq.heappop(arr))
+# print(heapq.heappop(arr))
